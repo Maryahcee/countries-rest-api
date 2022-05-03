a0fe323326df350111a37be17aacb2cdc503891c
MaryahKim <maryapitaz@gmail.com>
2022-05-03 13:32:56 +0300

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,21 +1,29 @@
 import { createStore } from 'vuex'
 import axios from 'axios'
 import serviceApi from '../apis/index'
+import theme from '../modules/theme'
 
 export default createStore({
     state:{
         countries: [ ],
+        theme:{},
     },
     mutations: {
-        // Mutation handlers (pass state object)
         SET_COUNTRIES(state, country) {
             console.log(country)
             state.countries = country.data
           },
+          SET_THEME(state, theme) {
+            state.theme = theme;
+            localStorage.theme = theme;
+        }
     },
     getters: {
         getAllCountries: state =>{
             return state.countries
+        },
+        getTheme: (state) => {
+            return state.theme;
         }
     },
     actions: {
@@ -29,7 +37,35 @@ export default createStore({
             .catch((error) => {
                 // handle error
                 console.log(error);
-              })
-       }
+              });
+       },
+       //darkmode option
+       initTheme({ commit }) {
+
+           const cachedTheme = localStorage.theme? localStorage.theme: false;
+          //  `true` if the user has set theme to `dark` on browser/OS
+          const userPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+          if (cachedTheme)
+            commit('SET_THEME', cashedTheme)
+          else if (userPrefersDark)
+            commit('SET_THEME', 'dark')
+          else
+            commit('SET_THEME', 'light')
+
+       },
+       toggleTheme({ commit }) {
+
+        switch (localStorage.theme) {
+            case 'light':
+                commit('SET_THEME', 'dark')
+                break;
+
+            default:
+                commit('SET_THEME', 'light')
+                break;
+        }
     }
+    },
+    
 })
